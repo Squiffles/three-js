67114688598f4e2da61a0b0ba93f28f3a374ae6a
unknown <martel.ssebastian@gmail.com>
2024-12-06 18:04:03 -0500

    [FEAT]: Added 3d text. [FIX]: Using scroll ratio to update the camera position on the z axis instead of increasing / decreasing the value depending on the scroll direction.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 import * as THREE from 'three';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+
 
 const c = document.querySelector(".canvas");
-const conW = c.clientWidth;
-const conH = c.clientHeight;
+const conW = c?.clientWidth;
+const conH = c?.clientHeight;
 
 // three variables:
 const fov = 75; // deg.
@@ -12,17 +15,17 @@ const farPoint = 1000;
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x121212);
 const camera = new THREE.PerspectiveCamera(fov, conW / conH, nearPoint, farPoint);
-camera.position.z = 5; // Moved a bit to avoid overlapping camera with object.
+camera.position.z = 10; // Moved a bit to avoid overlapping camera with object.
 
 // Camera animation:
-const cameraTopLimit = 5;
-const cameraBottomLimit = 12;
+const cameraTopLimit = 10;
+const cameraBottomLimit = 15;
 
 
 // Create the renderer:
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(conW, conH);
-c.appendChild(renderer.domElement);
+c?.appendChild(renderer.domElement);
 
 let sphere = null;
 function addSphere() {
@@ -46,26 +49,51 @@ addSphere();
 // Text --:
 
 // CANVAS that will be used as texture, later will be mapped:
-const textCanvas = document.createElement('canvas');
-const context = textCanvas.getContext('2d');
-textCanvas.width = window.innerWidth;
-textCanvas.height = window.innerHeight;
-context.font = '100px impact';
-context.fillStyle = 'white';
-context.textAlign = 'center';
-context.textBaseline = 'middle';
-context.fillText('TEXT SAMPLE', textCanvas.width / 2, textCanvas.height / 2);
+// const textCanvas = document.createElement('canvas');
+// const context = textCanvas.getContext('2d');
+// textCanvas.width = window.innerWidth;
+// textCanvas.height = window.innerHeight;
+// context.font = '100px impact';
+// context.fillStyle = 'white';
+// context.textAlign = 'center';
+// context.textBaseline = 'middle';
+// context.fillText('TEXT SAMPLE', textCanvas.width / 2, textCanvas.height / 2);
 
-// TEXTURE:
-const textTexture = new THREE.CanvasTexture(textCanvas);
+// // TEXTURE:
+// const textTexture = new THREE.CanvasTexture(textCanvas);
 
-// 
-const textGeometry = new THREE.PlaneGeometry(5, 2.5);
-const textMaterial = new THREE.MeshBasicMaterial({ map: textTexture, transparent: true });
-const textPlane = new THREE.Mesh(textGeometry, textMaterial);
-scene.add(textPlane);
-textPlane.position.set(0, 0, 5);
+// const textGeometry = new THREE.PlaneGeometry(5, 2.5);
+// const textMaterial = new THREE.MeshBasicMaterial({ map: textTexture, transparent: true });
+// const textPlane = new THREE.Mesh(textGeometry, textMaterial);
+// scene.add(textPlane);
+// textPlane.position.set(0, 0, 5);
 
+const text = "D I V E";
+const loader = new FontLoader();
+loader.load('https://unpkg.com/three@0.77.0/examples/fonts/helvetiker_regular.typeface.json', (font) => {
+
+    // Create the text geometry
+    const textGeometry = new TextGeometry(text, {
+        font: font,
+        size: .5,
+        height: .5,
+        curveSegments: 32,
+    });
+
+    // Create a standard material with red color and 50% gloss
+    const material = new THREE.MeshBasicMaterial({
+        color: 'white',
+        roughness: 0
+    });
+    const textMesh = new THREE.Mesh(textGeometry, material);
+    textMesh.position.set(-1, 0, 10);
+    scene.add(textMesh);
+});
+
+
+const directionalLight = new THREE.DirectionalLight(0xffd700, 1);
+directionalLight.position.set(1, 1, 1).normalize();
+scene.add(directionalLight);
 
 // ----------------------------------------------------------------
 function onWindowResize() {
@@ -92,24 +120,38 @@ const lenis = new Lenis({
     "autoRaf": true,
 });
 
+let update = true;
+let scrollPosTopLimit = 0;
+let scrollPosBottomLimit;
 function handleScroll(e) {
-    let update = true;
+    const scrollPosition = window.scrollY;
+    const scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollRatio = scrollPosition / scrollHeight;
 
     if (e?.direction === 1) {
+        if (!update && (scrollPosition > scrollPosTopLimit)) {
+            update = true;
+        }
+
         if (!update) return;
-        if (camera.position.z > cameraBottomLimit) {
+        if (camera.position.z >= cameraBottomLimit) {
             camera.position.z = cameraBottomLimit;
+            scrollPosBottomLimit = scrollPosition // Set the limit of the scroll only after the itas reached the bottom limit of the camera.
             update = false;
         } else {
-            camera.position.z += 0.05;
+            camera.position.z = (scrollRatio * 30) + 10;
         }
     } else {
+        if (!update && scrollPosBottomLimit) {
+            update = true;
+        }
+
         if (!update) return;
-        if (camera.position.z < cameraTopLimit) {
+        if (camera.position.z <= cameraTopLimit) {
             camera.position.z = cameraTopLimit;
             update = false;
         } else {
-            camera.position.z -= 0.05;
+            camera.position.z = (scrollRatio * 30) + 10;
         }
     }
 }
